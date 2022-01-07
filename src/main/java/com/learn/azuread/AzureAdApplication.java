6b6c3d40b7ffa47db1628a726d6e62cbb592c65e
Himanshu Rajvanshi <himanshu.rajvanshi@synoriq.in>
2022-01-07 09:59:45 +0530

    Initial commit

---
+++ src/main/java/com/learn/azuread/AzureAdApplication.java
@@ -0,0 +1,18 @@
+package com.learn.azuread;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.authentication.configuration.EnableGlobalAuthentication;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+@SpringBootApplication
+public class AzureAdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AzureAdApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/learn/azuread/security/container/HelloController.java
@@ -0,0 +1,16 @@
+package com.learn.azuread.security.container;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("Admin")
+    @ResponseBody
+    @PreAuthorize("hasAuthority('APPROLE_Admin')")
+    public String Admin() {
+        return "Admin message";
+    }
+}
